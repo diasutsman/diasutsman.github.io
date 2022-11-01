e874554b82f812095d743bb1dbafd880eb2bf2e6
Dias Utsman <utsmand91@gmail.com>
2022-11-01 10:57:22 +0700

    Add animation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
-const scrollDown = document.querySelector('.scroll-down')
-scrollDown.addEventListener('click', () => {
-    scrollTo(0, innerHeight)
-}) 
+function getMyGithubData() {
+    return fetch('https://api.github.com/users/diasutsman', {
+        headers: {
+            'Accept': 'application/vnd.github+json',
+            'Authorization': `Bearer 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`
+        }
+    })
+        .then(res => res.json())
+        .then(data => data['public_repos'])
+}
+
+async function updateUI() {
+    const projectDone = document.getElementById('projectDone')
+
+    projectDone.innerText = await getMyGithubData()
+
+    removeLoading()
+}
+
+function removeLoading() {
+    const loading = document.querySelector('.loading-container')
+    loading.classList.add('fade-out')
+
+    loading.addEventListener('animationend', () => loading.remove())
+}
+
+updateUI()
