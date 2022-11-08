84d900aabbd77ce712f4bb1a2ac8cbc59c596d3b
Dias Utsman <utsmand91@gmail.com>
2022-11-08 10:25:45 +0700

    Use bootstrap

--- js/data.js
+++ js/data.js
@@ -1,118 +1,83 @@
-const certsSlide = new Swiper('.certs-swiper', {
-    slidesPerView: 'auto',
-    spaceBetween: 20,
-    loop: true,
-    speed: 1500,
-    autoplay: {
-        delay: 1,
-        disableOnInteraction: false
-    },
-    breakpoints: {
-        // when window width is >= 700px
-        700: {
-            slidesPerView: 2,
-            spaceBetween: 20
-        },
-    },
-});
+function getProjectsCount() {
+    return fetch('https://api.github.com/users/diasutsman', {
+        headers: {
+            'Accept': 'application/vnd.github+json',
+            //'Authorization': `Bearer ${githubApiKey}`
+        }
+    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            return data['public_repos']
+        })
+}
 
-const certsImages = [
-    "./img/dicoding/android-pemula.webp",
-    "./img/dicoding/architecting-on-aws.webp",
-    "./img/dicoding/aws-partitioner.webp",
-    "./img/dicoding/back-end-aws.webp",
-    "./img/dicoding/back-end-fundamental.webp",
-    "./img/dicoding/back-end-google-cloud.webp",
-    "./img/dicoding/c.webp",
-    "./img/dicoding/flutter.webp",
-    "./img/dicoding/front-end-pemula.webp",
-    "./img/dicoding/java.webp",
-    "./img/dicoding/js-dicoding-cert.webp",
-    "./img/dicoding/solid.webp",
-];
+function getRepoCountByLang(lang) {
+    return fetch(`https://api.github.com/search/repositories?q=user:diasutsman%20language:${lang}`, {
+        headers: {
+            'Accept': 'application/vnd.github+json',
+            //'Authorization': `Bearer ${githubApiKey}`
+        }
+    })
+        .then(res => res.json())
+        .then(data => {
+            return data['total_count']
+        })
+}
 
-certsImages.forEach((certsImage) => {
-    certsSlide.appendSlide(`
-        <div class="swiper-slide">
-            <img src="${certsImage}" alt="certs-image">
-        </div>
-    `);
-})
 
-const skillsContent = document.querySelector('#skills .content');
+function getWebsCount() {
+    return getRepoCountByLang('css')
+}
 
-const skills = [
-    {
-        name: 'Node Js',
-        percentage: 90,
-        image: './img/skills/nodejs.webp',
-        desc: 'A runtime environment for running Javascript outside browser. Node Js is used to build server-side applications.',
-    },
-    {
-        name: 'Website',
-        percentage: 95,
-        image: './img/skills/website.webp',
-        desc: 'I can build a website using HTML, CSS, and Javascript. I can also build a website using a framework like React Js.',
-    },
-    {
-        name: 'React Js',
-        percentage: 75,
-        image: './img/skills/react.webp',
-        desc: 'A Javascript library for building user interfaces. React Js is used to build client-side applications.',
-    },
-    {
-        name: 'Express',
-        percentage: 65,
-        image: './img/skills/express.webp',
-        desc: 'A web framework for Node Js. Express is used to build server-side applications.',
-    },
-    {
-        name: 'Hapi Js',
-        percentage: 75,
-        image: './img/skills/hapi.webp',
-        desc: 'A web framework for Node Js. Hapi Js is used to build server-side applications.',
-    },
-    {
-        name: 'Android',
-        percentage: 80,
-        image: './img/skills/android.webp',
-        desc: 'I can build an Android application using Kotlin. I can also build an Android application using libraries like Retrofit, Glide, and Room.',
-    },
-    {
-        name: 'Flutter',
-        percentage: 45,
-        image: './img/skills/flutter.webp',
-        desc: 'A framework for building cross-platform applications. Flutter is used to build client-side applications.',
-    },
-    {
-        name: 'Firebase',
-        percentage: 35,
-        image: './img/skills/firebase.webp',
-        desc: 'A service from google to provide database and authentication for web and mobile applications.',
-    },
-    {
-        name: 'MongoDB',
-        percentage: 50,
-        image: './img/skills/mongodb.webp',
-        desc: 'A NoSQL database. MongoDB is used to store data in JSON-like documents.',
-    }
+function getMobileAppsCount() {
+    return getRepoCountByLang('kotlin')
+}
 
-]
+function getFlutterAppsCount() {
+    return getRepoCountByLang('dart')
+}
 
-skillsContent.innerHTML = skills.map(skill => {
-    return `<div class="flip-card">
-    <div class="flip-card-inner">
-      <div class="flip-card-front">
-        <img src="${skill.image}" alt="${skill.name}">
-        <p>${skill.name}</p>
-      </div>
-      <div class="flip-card-back">
-        <h1>${skill.name}</h1>
-        <p>${skill.desc}</p>
-        <div class="percentage">
-          <div class="progress" style="width: ${skill.percentage}%"></div>
-        </div>
-      </div>
-    </div>
-  </div>`
-}).join('');
+function removeLoading() {
+    const loading = document.querySelector('.loading-container')
+    loading.classList.add('fade-out')
+
+    loading.addEventListener('animationend', () => loading.remove())
+}
+
+async function typeAfterResolve(el, promise) {
+    new TypeIt(el, {
+        speed: 100,
+        afterComplete: async (ins) => {
+            ins.destroy()
+            let instance, data
+            try {
+                data = await promise
+                instance = new TypeIt(el, {
+                    speed: 100,
+                    afterComplete: ins => ins.destroy(),
+                })
+                instance.type(data)
+            } catch (error) {
+                instance.type('Cannot count')
+            }
+            instance.go()
+        }
+    })
+        .type('counting...')
+        .delete()
+        .go()
+}
+
+function typeIt() {
+    // Type it
+    new TypeIt('#typeIt', {
+        speed: 100,
+        afterComplete: (instance) => {
+            instance.destroy()
+            new TypeIt('#typeIt1', {
+                speed: 30,
+            }).go()
+        }
+    }).go()
+}
