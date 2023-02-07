e69da6060ae63e195858c9a839712ee618950f37
Dias Utsman <utsmand91@gmail.com>
2023-02-07 07:58:32 +0700

    Add code for adding portfolio from Github API

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,295 +5,319 @@
  * License: https://bootstrapmade.com/license/
  */
 (async function () {
-	"use strict";
+  "use strict";
 
-	/**
-	 * Easy selector helper function
-	 */
-	const select = (el, all = false) => {
-		el = el.trim();
-		if (all) {
-			return [...document.querySelectorAll(el)];
-		} else {
-			return document.querySelector(el);
-		}
-	};
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
 
-	/**
-	 * Easy event listener function
-	 */
-	const on = (type, el, listener, all = false) => {
-		let selectEl = select(el, all);
-		if (selectEl) {
-			if (all) {
-				selectEl.forEach((e) => e.addEventListener(type, listener));
-			} else {
-				selectEl.addEventListener(type, listener);
-			}
-		}
-	};
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach((e) => e.addEventListener(type, listener));
+      } else {
+        selectEl.addEventListener(type, listener);
+      }
+    }
+  };
 
-	/**
-	 * Easy on scroll event listener
-	 */
-	const onscroll = (el, listener) => {
-		el.addEventListener("scroll", listener);
-	};
+  /**
+   * Easy on scroll event listener
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener("scroll", listener);
+  };
 
-	/**
-	 * Navbar links active state on scroll
-	 */
-	let navbarlinks = select("#navbar .scrollto", true);
-	const navbarlinksActive = () => {
-		let position = window.scrollY + 200;
-		navbarlinks.forEach((navbarlink) => {
-			if (!navbarlink.hash) return;
-			let section = select(navbarlink.hash);
-			if (!section) return;
-			if (
-				position >= section.offsetTop &&
-				position <= section.offsetTop + section.offsetHeight
-			) {
-				navbarlink.classList.add("active");
-			} else {
-				navbarlink.classList.remove("active");
-			}
-		});
-	};
-	window.addEventListener("load", navbarlinksActive);
-	onscroll(document, navbarlinksActive);
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select("#navbar .scrollto", true);
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200;
+    navbarlinks.forEach((navbarlink) => {
+      if (!navbarlink.hash) return;
+      let section = select(navbarlink.hash);
+      if (!section) return;
+      if (
+        position >= section.offsetTop &&
+        position <= section.offsetTop + section.offsetHeight
+      ) {
+        navbarlink.classList.add("active");
+      } else {
+        navbarlink.classList.remove("active");
+      }
+    });
+  };
+  window.addEventListener("load", navbarlinksActive);
+  onscroll(document, navbarlinksActive);
 
-	/**
-	 * Scrolls to an element with header offset
-	 */
-	const scrollto = (el) => {
-		let elementPos = select(el).offsetTop;
-		window.scrollTo({
-			top: elementPos,
-			behavior: "smooth",
-		});
-	};
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let elementPos = select(el).offsetTop;
+    window.scrollTo({
+      top: elementPos,
+      behavior: "smooth",
+    });
+  };
 
-	/**
-	 * Back to top button
-	 */
-	let backtotop = select(".back-to-top");
-	if (backtotop) {
-		const toggleBacktotop = () => {
-			if (window.scrollY > 100) {
-				backtotop.classList.add("active");
-			} else {
-				backtotop.classList.remove("active");
-			}
-		};
-		window.addEventListener("load", toggleBacktotop);
-		onscroll(document, toggleBacktotop);
-	}
+  /**
+   * Back to top button
+   */
+  let backtotop = select(".back-to-top");
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add("active");
+      } else {
+        backtotop.classList.remove("active");
+      }
+    };
+    window.addEventListener("load", toggleBacktotop);
+    onscroll(document, toggleBacktotop);
+  }
 
-	/**
-	 * Mobile nav toggle
-	 */
-	on("click", ".mobile-nav-toggle", function (e) {
-		select("body").classList.toggle("mobile-nav-active");
-		this.classList.toggle("bi-list");
-		this.classList.toggle("bi-x");
-	});
+  /**
+   * Mobile nav toggle
+   */
+  on("click", ".mobile-nav-toggle", function (e) {
+    select("body").classList.toggle("mobile-nav-active");
+    this.classList.toggle("bi-list");
+    this.classList.toggle("bi-x");
+  });
 
-	/**
-	 * Scrool with ofset on links with a class name .scrollto
-	 */
-	on(
-		"click",
-		".scrollto",
-		function (e) {
-			if (select(this.hash)) {
-				e.preventDefault();
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on(
+    "click",
+    ".scrollto",
+    function (e) {
+      if (select(this.hash)) {
+        e.preventDefault();
 
-				let body = select("body");
-				if (body.classList.contains("mobile-nav-active")) {
-					body.classList.remove("mobile-nav-active");
-					let navbarToggle = select(".mobile-nav-toggle");
-					navbarToggle.classList.toggle("bi-list");
-					navbarToggle.classList.toggle("bi-x");
-				}
-				scrollto(this.hash);
-			}
-		},
-		true
-	);
+        let body = select("body");
+        if (body.classList.contains("mobile-nav-active")) {
+          body.classList.remove("mobile-nav-active");
+          let navbarToggle = select(".mobile-nav-toggle");
+          navbarToggle.classList.toggle("bi-list");
+          navbarToggle.classList.toggle("bi-x");
+        }
+        scrollto(this.hash);
+      }
+    },
+    true
+  );
 
-	/**
-	 * Scroll with ofset on page load with hash links in the url
-	 */
-	window.addEventListener("load", () => {
-		if (window.location.hash) {
-			if (select(window.location.hash)) {
-				scrollto(window.location.hash);
-			}
-		}
-	});
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener("load", () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash);
+      }
+    }
+  });
 
-	/**
-	 * Preloader
-	 */
-	let preloader = select("#preloader");
-	if (preloader) {
-		window.addEventListener("load", () => {
-			preloader.remove();
-		});
-	}
+  /**
+   * Preloader
+   */
+  let preloader = select("#preloader");
+  if (preloader) {
+    window.addEventListener("load", () => {
+      preloader.remove();
+    });
+  }
 
-	/**
-	 * Hero type effect
-	 */
-	const typed = select(".typed");
-	if (typed) {
-		let typed_strings = typed.getAttribute("data-typed-items");
-		typed_strings = typed_strings.split(",");
-		new Typed(".typed", {
-			strings: typed_strings,
-			loop: true,
-			typeSpeed: 100,
-			backSpeed: 50,
-			backDelay: 2000,
-		});
-	}
+  /**
+   * Hero type effect
+   */
+  const typed = select(".typed");
+  if (typed) {
+    let typed_strings = typed.getAttribute("data-typed-items");
+    typed_strings = typed_strings.split(",");
+    new Typed(".typed", {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000,
+    });
+  }
 
-	/**
-	 * Skills animation
-	 */
-	let skilsContent = select(".skills-content");
-	if (skilsContent) {
-		new Waypoint({
-			element: skilsContent,
-			offset: "80%",
-			handler: function (direction) {
-				let progress = select(".progress .progress-bar", true);
-				progress.forEach((el) => {
-					el.style.width = el.getAttribute("aria-valuenow") + "%";
-				});
-			},
-		});
-	}
+  /**
+   * Skills animation
+   */
+  let skilsContent = select(".skills-content");
+  if (skilsContent) {
+    new Waypoint({
+      element: skilsContent,
+      offset: "80%",
+      handler: function (direction) {
+        let progress = select(".progress .progress-bar", true);
+        progress.forEach((el) => {
+          el.style.width = el.getAttribute("aria-valuenow") + "%";
+        });
+      },
+    });
+  }
 
-	/**
-	 * Porfolio isotope and filter
-	 */
-	window.addEventListener("load", () => {
-		let portfolioContainer = select(".portfolio-container");
-		if (portfolioContainer) {
-			let portfolioIsotope = new Isotope(portfolioContainer, {
-				itemSelector: ".portfolio-item",
-			});
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener("load", async () => {
+    let portfolioContainer = select(".portfolio-container");
+    portfolioContainer.innerHTML =
+      await fetch("https://api.github.com/search/repositories?q=user:diasutsman%20topic:project&per_page=100")
+        .then((response) => response.json())
+        .then((data) => data.items
+          .filter(({ topics }) => topics.find(topic => ['web', 'android', 'back-end', 'flutter'].includes(topic)))
+          .map((portfolio) => ({
+            ...portfolio,
+            category: portfolio.topics.find(topic => ['web', 'android', 'back-end', 'flutter'].includes(topic)),
+            name: normalizeText.normalizeName(portfolio.name.replace(/[-_]/g, ' ').replace(/[a-z][A-Z]/g, c => `${c[0]} ${c[1]}`)),
+          }))
+          .map((portfolio) => portfolioTemplate(portfolio)).join("")
+        );
 
-			let portfolioFilters = select("#portfolio-flters li", true);
+    select('.portfolio-wrap img', true).forEach((img) => {
+      img.onload = () => new Isotope(portfolioContainer, {
+        itemSelector: ".portfolio-item",
+      }).arrange({ filter: '*' });;
+    });
 
-			on(
-				"click",
-				"#portfolio-flters li",
-				function (e) {
-					e.preventDefault();
-					portfolioFilters.forEach(function (el) {
-						el.classList.remove("filter-active");
-					});
-					this.classList.add("filter-active");
 
-					portfolioIsotope.arrange({
-						filter: this.getAttribute("data-filter"),
-					});
-					portfolioIsotope.on("arrangeComplete", function () {
-						AOS.refresh();
-					});
-				},
-				true
-			);
-		}
-	});
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: ".portfolio-item",
+      });
 
-	/**
-	 * Initiate portfolio lightbox
-	 */
-	const portfolioLightbox = GLightbox({
-		selector: ".portfolio-lightbox",
-	});
+      let portfolioFilters = select("#portfolio-filters li", true);
 
-	/**
-	 * Initiate portfolio details lightbox
-	 */
-	const portfolioDetailsLightbox = GLightbox({
-		selector: ".portfolio-details-lightbox",
-		width: "90%",
-		height: "90vh",
-	});
+      on(
+        "click",
+        "#portfolio-filters li",
+        function (e) {
+          e.preventDefault();
+          portfolioFilters.forEach(function (el) {
+            el.classList.remove("filter-active");
+          });
+          this.classList.add("filter-active");
 
-	/**
-	 * Portfolio details slider
-	 */
-	new Swiper(".portfolio-details-slider", {
-		speed: 400,
-		loop: true,
-		autoplay: {
-			delay: 5000,
-			disableOnInteraction: false,
-		},
-		pagination: {
-			el: ".swiper-pagination",
-			type: "bullets",
-			clickable: true,
-		},
-	});
+          portfolioIsotope.arrange({
+            filter: this.getAttribute("data-filter"),
+          });
+          portfolioIsotope.on("arrangeComplete", function () {
+            AOS.refresh();
+          });
+        },
+        true
+      );
+    }
 
-	/**
-	 * Testimonials slider
-	 */
-	new Swiper(".testimonials-slider", {
-		speed: 600,
-		loop: true,
-		autoplay: {
-			delay: 5000,
-			disableOnInteraction: false,
-		},
-		slidesPerView: "auto",
-		pagination: {
-			el: ".swiper-pagination",
-			type: "bullets",
-			clickable: true,
-		},
-	});
+    /**
+   * Initiate portfolio lightbox
+   */
+    const portfolioLightbox = GLightbox({
+      selector: ".portfolio-lightbox",
+    });
 
-	/**
-	 * Animation on scroll
-	 */
-	window.addEventListener("load", () => {
-		AOS.init({
-			duration: 1000,
-			easing: "ease-in-out",
-			once: true,
-			mirror: false,
-		});
-	});
+    /**
+     * Initiate portfolio details lightbox
+     */
+    const portfolioDetailsLightbox = GLightbox({
+      selector: ".portfolio-details-lightbox",
+      width: "90%",
+      height: "90vh",
+    });
+  });
 
-	/**
-	 * calculate age
-	 */
 
-	const calculateAge = (birthday) => {
-		let ageDifMs = Date.now() - birthday.getTime();
-		let ageDate = new Date(ageDifMs);
-		return Math.abs(ageDate.getUTCFullYear() - 1970);
-	};
 
-	/**
-	 * calculate days
-	 */
-	const calculateDays = (date) => {
-		const today = new Date();
-		const difference = today - new Date(date);
-		return Math.floor(difference / (1000 * 60 * 60 * 24));
-	};
+  /**
+   * Portfolio details slider
+   */
+  new Swiper(".portfolio-details-slider", {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+  });
 
-	// skill template
-	const skillTemplate = (skill) => `
+  /**
+   * Testimonials slider
+   */
+  new Swiper(".testimonials-slider", {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: "auto",
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+  });
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener("load", () => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,
+    });
+  });
+
+  /**
+   * calculate age
+   */
+
+  const calculateAge = (birthday) => {
+    let ageDifMs = Date.now() - birthday.getTime();
+    let ageDate = new Date(ageDifMs);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  };
+
+  /**
+   * calculate days
+   */
+  const calculateDays = (date) => {
+    const today = new Date();
+    const difference = today - new Date(date);
+    return Math.floor(difference / (1000 * 60 * 60 * 24));
+  };
+
+  /**
+   * Template for skills section
+   */
+  const skillTemplate = (skill) => `
   <div class="progress col-lg-6">
     <span class="skill">${skill.name} <i class="val">${skill.percentage}%</i></span>
     <div class="progress-bar-wrap">
@@ -303,31 +327,52 @@
   </div>
   `;
 
-	/**
-	 * Initiate Pure Counter
-	 */
-	new PureCounter();
+  const portfolioTemplate = (portfolio) => {
+    return `
+    <div class="col-lg-4 col-md-6 portfolio-item filter-${portfolio.category}">
+      <div class="portfolio-wrap">
+        <img src="${portfolio.category === 'web' ? `https://api.screenshotmachine.com/?key=715404&url=${portfolio.homepage}&dimension=1920x1080` : 'assets/img/portfolio/portfolio-1.webp'}" class="img-fluid" alt="">
+        <div class="portfolio-info">
+          <h4>${portfolio.name}</h4>
+          <p>${portfolio.category.replace(/[-_]/g, ' ')}</p>
+          <div class="portfolio-links">
+            <a href="assets/img/portfolio/portfolio-1.webp" data-gallery="portfolioGallery" class="portfolio-lightbox" title="
+              ${portfolio.name}
+            "><i class="bx bx-plus"></i></a>
+            <a href="${portfolio.html_url}" target="_blank"><i class="bx bx-link"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    `
+  }
 
-	/**
-	 * Initiate elements that dynamically changes
-	 */
+  /**
+   * Initiate Pure Counter
+   */
+  new PureCounter();
 
-	// my age
-	select("#myAge").innerText = calculateAge(new Date(2006, 7, 22));
+  /**
+   * Initiate elements that dynamically changes
+   */
 
-	// hours of code
-	select("#hoursOfCode").dataset.purecounterEnd =
-		calculateDays(new Date(2020, 6, 4)) * 3;
+  // my age
+  select("#myAge").innerText = calculateAge(new Date(2006, 7, 22));
 
-	// projects count
-	select("#projects").dataset.purecounterEnd = await fetch(
-		"https://api.github.com/search/repositories?q=user:diasutsman%20topic:project"
-	)
-		.then((response) => response.json())
-		.then((data) => data.total_count);
+  // hours of code
+  select("#hoursOfCode").dataset.purecounterEnd =
+    calculateDays(new Date(2020, 6, 4)) * 3;
 
-	// skills
-	select(".skills-content").innerHTML = await fetch("/assets/json/skills.json")
-		.then((response) => response.json())
-		.then((data) => data.map((skill) => skillTemplate(skill)).join(""));
+  // projects count
+  select("#projects").dataset.purecounterEnd = await fetch(
+    "https://api.github.com/search/repositories?q=user:diasutsman%20topic:project"
+  )
+    .then((response) => response.json())
+    .then((data) => data.total_count);
+
+  // skills
+  select(".skills-content").innerHTML = await fetch("/assets/json/skills.json")
+    .then((response) => response.json())
+    .then((data) => data.map((skill) => skillTemplate(skill)).join(""));
+
 })();
